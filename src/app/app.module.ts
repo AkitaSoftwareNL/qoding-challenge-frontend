72f81cc19d064572911a96a52d5c7ab12794a4f8
BeingBright <BrightonvandenEnd@gmail.com>
2019-11-21 12:16:58 +0100

    renamed question to open-question

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
+import { OpenQuestionComponent } from './open-question/open-question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionComponent
+    OpenQuestionComponent
   ],
   imports: [
     BrowserModule,
--- src/app/open-question/open-question.component.spec.ts
+++ src/app/open-question/open-question.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QuestionComponent } from './question.component';
+import { OpenQuestionComponent } from './open-question.component';
 
-describe('QuestionComponent', () => {
-  let component: QuestionComponent;
-  let fixture: ComponentFixture<QuestionComponent>;
+describe('OpenQuestionComponent', () => {
+  let component: OpenQuestionComponent;
+  let fixture: ComponentFixture<OpenQuestionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ QuestionComponent ]
+      declarations: [ OpenQuestionComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(QuestionComponent);
+    fixture = TestBed.createComponent(OpenQuestionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
