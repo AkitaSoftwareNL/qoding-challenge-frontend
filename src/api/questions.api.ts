95a76142008f3ad05615d86032d630d038719550
BeingBright <BrightonvandenEnd@gmail.com>
2019-11-25 10:37:00 +0100

    Update questions.api.ts

--- src/api/questions.api.ts
+++ src/api/questions.api.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Campaign } from 'src/classes/campaign';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class QuestionsApi {
