9147c9889c5833b5ddf3651fe05618f01fc51908
BeingBright <BrightonvandenEnd@gmail.com>
2019-12-12 19:08:17 +0100

    Refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/conference/1', pathMatch: 'full' },
+  { path: 'conference/:id', component: QuizComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/quiz/quiz.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Question } from 'src/classes/question';
+import { QuestionsService } from 'src/api/questionsService';
+import { Campaign } from 'src/classes/campaign';
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css']
+})
+export class QuizComponent {
+  routeParams: Params;
+  currentQuestionIndex = 0;
+  currentQuestion: Question;
+  campaign: Campaign;
+  campaignID: number;
+
+  constructor(private route: ActivatedRoute, private questionsService: QuestionsService) {
+    this.route.params.subscribe(params => {
+      this.playCampagne(params.id);
+    });
+  }
+
+  playCampagne(id: number) {
+    this.campaignID = id;
+    this.campaign = null;
+    this.questionsService.get(id).subscribe(campaign => {
+      this.currentQuestionIndex = 0;
+      this.campaign = campaign;
+      this.currentQuestion = this.campaign.questions[0];
+    }, error => {
+      console.log(error);
+      alert('Oeps! Er ging wat mis');
+    });
+  }
+
+  onNext() {
+    if (this.campaign.questions.length - 1 <= this.currentQuestionIndex) {
+      this.send();
+    } else {
+      this.currentQuestionIndex += 1;
+      this.currentQuestion = this.campaign.questions[this.currentQuestionIndex];
+    }
+  }
+
+  send() {
+    this.questionsService.post(this.campaignID, this.campaign)
+      .subscribe(succes => alert('Dank u voor het meedoen aan ' + this.campaign.campaignName), error => {
+        console.error(error);
+        alert('Oeps! Er ging wat mis');
+      });
+    this.campaign = null;
+  }
+
+}
