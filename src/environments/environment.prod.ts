980c78c0f0640448d2516173119bf5f09139e07d
Nick <nick.braks@live.nl>
2019-12-20 09:30:34 +0100

    added firebase config to environment production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyB1Ns7yE9vtgiqKFbGHWc-SrWSuBqg_1AA',
+    authDomain: 'qodingchallenge-9708d.firebaseapp.com',
+    databaseURL: 'https://qodingchallenge-9708d.firebaseio.com',
+    projectId: 'qodingchallenge-9708d',
+    storageBucket: 'qodingchallenge-9708d.appspot.com',
+    messagingSenderId: '862742111394',
+    appId: '1:862742111394:web:a9b2962582a33d0cce23fb'
+  },
   production: true
 };
