45ab86438f5f87cf018bf6621224a8ec5e3454b6
BeingBright <BrightonvandenEnd@gmail.com>
2020-01-16 16:20:37 +0100

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,18 @@
-import {Component, OnInit, Output} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
-import {EventEmitter} from 'events';
-import {Participant} from '../../classes/participant';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Subscription} from 'rxjs';
-import {ParticipantService} from '../../api/participant.service';
-import {isNull} from 'util';
-import {QuizComponent} from '../quiz/quiz.component';
-import {AuthenticationService} from '../../api/authentication.service';
+import { Component, OnInit, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { EventEmitter } from 'events';
+import { Participant } from '../../classes/participant';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ParticipantService } from '../../api/participant.service';
+import { isNull } from 'util';
+import { QuizComponent } from '../quiz/quiz.component';
+import { AuthenticationService } from '../../api/authentication.service';
 import 'rxjs-compat/add/observable/fromPromise';
-import {ToastrService} from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
 import validate = WebAssembly.validate;
-import {QuestionsService} from "../../api/questionsService";
-import {timeout} from "rxjs/operators";
+import { QuestionsService } from "../../api/questionsService";
+import { timeout } from "rxjs/operators";
 
 @Component({
   selector: 'app-login',
@@ -37,11 +37,11 @@ export class LoginComponent implements OnInit {
   participantDTO = new Participant();
 
   constructor(private formBuilder: FormBuilder, private route: ActivatedRoute,
-              private participantService: ParticipantService, private router: Router,
-              private quizComponent: QuizComponent,
-              private authenticationService: AuthenticationService,
-              private questionService: QuestionsService,
-              private toast: ToastrService) {
+    private participantService: ParticipantService, private router: Router,
+    private quizComponent: QuizComponent,
+    private authenticationService: AuthenticationService,
+    private questionService: QuestionsService,
+    private toast: ToastrService) {
   }
 
   ngOnInit() {
@@ -51,8 +51,8 @@ export class LoginComponent implements OnInit {
         this.quizComponent.campaign = campaign;
       },
         error => {
-        console.log(error);
-        this.toast.error(error.valueOf().error.message, 'Campaign not found', {
+          console.log(error);
+          this.toast.error(error.valueOf().error.message, 'Campaign not found', {
             timeOut: 60000
           });
         });
@@ -79,7 +79,7 @@ export class LoginComponent implements OnInit {
   googleLogin() {
     this.authenticationService.doGoogleLogin().then(() => {
       this.createParticipantFromSocial();
-      },
+    },
       err => {
         console.log(err);
       });
@@ -87,21 +87,23 @@ export class LoginComponent implements OnInit {
 
   facebookLogin() {
     this.authenticationService.doFacebookLogin().then(() => {
-        this.createParticipantFromSocial();
-      },
+      this.createParticipantFromSocial();
+    },
       err => {
         console.log(err);
       });
   }
 
   private createParticipantFromSocial(): void {
-    const currentUser = this.authenticationService.afAuth.auth.currentUser;
-    const name = currentUser.displayName.split(' ');
 
     this.authenticationService.afAuth.user.subscribe(result => {
-      result.providerData.forEach(test => {
-        this.participantDTO.phonenumber = test.phoneNumber;
-        this.participantDTO.email = test.email;
+
+      const currentUser = this.authenticationService.afAuth.auth.currentUser;
+      const name = currentUser.displayName.split(' ');
+      result.providerData.forEach(data => {
+        if (this.participantDTO.email === data.email) { return; }
+        this.participantDTO.phonenumber = data.phoneNumber;
+        this.participantDTO.email = data.email;
         this.participantDTO.firstname = name[0];
         this.participantDTO.insertion = (name.length > 2) ? name[1] + name[name.length - 2] : null;
         this.participantDTO.lastname = (name.length > 1) ? name[name.length - 1] : '';
@@ -114,14 +116,15 @@ export class LoginComponent implements OnInit {
   private callPost(participant: Participant) {
     this.participantService.postNewParticipant(participant, this.quizComponent.campaignID)
       .subscribe(succes => {
-          this.toast.info('Beste ' + participant.firstname + ' ' + ((isNull(participant.insertion)) ? '' : participant.insertion) +
-            ' ' + participant.lastname + ', succes met de quiz!');
-          this.quizComponent.userUUID = succes.participantID;
-          this.quizComponent.playCampaign(this.quizComponent.campaignID);
-        },
+        this.toast.info('Beste ' + participant.firstname + ' ' + ((isNull(participant.insertion)) ? '' : participant.insertion) +
+          ' ' + participant.lastname + ', succes met de quiz!');
+        this.quizComponent.userUUID = succes.participantID;
+        this.quizComponent.playCampaign(this.quizComponent.campaignID);
+      },
         error => {
           this.toast.warning(error.valueOf().error.message);
           console.log(error.valueOf().error.message);
+          this.participantDTO = new Participant();
         });
   }
 }
