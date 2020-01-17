95a4832f2557a280d2784129a9035d04260e27af
jeroen <jerrik1998@gmail.com>
2020-01-17 13:40:51 +0100

    Translated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent implements OnInit {
       },
         error => {
           console.log(error);
-          this.toast.error(error.valueOf().error.message, 'Campaign not found', {
+          this.toast.error(error.valueOf().error.message, 'Campagne niet beschikbaar', {
             timeOut: 60000
           });
         });
